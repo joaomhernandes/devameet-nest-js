52d0a7ebeceff509ff31fe8591232bacb2b718a5
João Maurício Hernandes Carrenho <78742654+joaomhernandes@users.noreply.github.com>
2023-03-14 15:55:57 -0300

    Create Auth Module
    Create mocked auth module

---
+++ src/auth/helpers/messages.helper.ts
@@ -0,0 +1,5 @@
+export const MessagesHelper = {
+  AUTH_LOGIN_NOT_FOUD: 'Login informado não é válido',
+  AUTH_PASSWORD_NOT_FOUD: 'Senha informado não é válida',
+  AUTH_PASSWORD_OR_EMAIL_NOT_FOUND: 'E-mail e/ou senha inválidos!',
+};
