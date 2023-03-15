e173bdd396a4b2e8b36f8be3ec27ece9ac2aa20b
João Maurício Hernandes Carrenho <78742654+joaomhernandes@users.noreply.github.com>
2023-03-15 10:02:44 -0300

    Create Register Feature
    Create Register Feature and user schema

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,18 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import { RegisterDto } from 'src/user/dtos/register.dto';
+import { UserMessagesHelper } from 'src/user/helpers/messages.helper';
+import { UserService } from 'src/user/user.service';
 import { LoginDto } from './dtos/login.dto';
 import { MessagesHelper } from './helpers/messages.helper';
 
 @Injectable()
 export class AuthService {
+  private logger = new Logger(AuthService.name);
+
+  constructor(private readonly userServiçe: UserService) {}
+
   login(dto: LoginDto) {
+    this.logger.debug('Login - started');
     if (dto.login !== 'teste@teste.com' || dto.password !== 'teste@123') {
       throw new BadRequestException(
         MessagesHelper.AUTH_PASSWORD_OR_EMAIL_NOT_FOUND,
@@ -12,5 +20,13 @@ export class AuthService {
     }
 
     return dto;
+  }
+
+  async register(dto: RegisterDto) {
+    this.logger.debug('Register - started');
+    if (await this.userServiçe.existsByEmail(dto.email)) {
+      throw new BadRequestException(UserMessagesHelper.REGISTER_EMAIL_FOUND);
+    }
+    await this.userServiçe.create(dto);
   }
 }
---
+++ src/user/helpers/messages.helper.ts
@@ -0,0 +1,8 @@
+export const UserMessagesHelper = {
+  USER_LOGIN_OR_EMAIL_NOT_FOUND: 'Email e/ou senha inválidos',
+  REGISTER_NAME_NOT_VALID: 'Nome informado não é válido',
+  REGISTER_EMAIL_NOT_VALID: 'Email informado não é válido',
+  REGISTER_PASSWORD_NOT_VALID:
+    'A Senha deve ter de 4 a 12 caracteres e conter maiúsculas, minúsculas, números e caracteres especiais',
+  REGISTER_EMAIL_FOUND: 'Já existe uma conta com o email informado',
+};
