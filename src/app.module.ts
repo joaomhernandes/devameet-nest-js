0e544d3ecfd0bfd84a80982eda4239e2c3734a8a
João Maurício Hernandes Carrenho <78742654+joaomhernandes@users.noreply.github.com>
2023-04-23 05:59:07 -0300

    Created Guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { JwtAuthGuard } from './auth/guards/jwt.guard';
+import { APP_GUARD } from '@nestjs/core';
 @Module({
   imports: [
     ConfigModule.forRoot(),
@@ -11,6 +13,8 @@ import { UserModule } from './user/user.module';
     UserModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {provide: APP_GUARD, useClass: JwtAuthGuard }
+  ],
 })
 export class AppModule {}
